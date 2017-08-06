8cc482637ffaf13d558421390614941aa35a6b20
Jan Basałaj <janbasalaj@gmail.com>
2017-08-06 22:14:55 +0200

    First commit

---
+++ DiscordFunBot/Program.cs
@@ -0,0 +1,54 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public class Program
+    {
+
+        private DiscordSocketClient _client;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+
+            _client.Log += Log;
+
+            _client.MessageReceived += MessageReceived;
+
+     
+
+
+            string token = "MzQzODI0ODgzMTc3ODgxNjAw.DGj34w.eEUYp_1zTImHv1eY6GJRbZNO6h8"; // Remember to keep this private!
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "Halo?")
+            {
+                await message.Channel.SendMessageAsync("Kermit ciota");
+            }
+        }
+
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
